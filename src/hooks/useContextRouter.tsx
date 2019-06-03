752d4207771960d619f934ed9b53621f0723a5b9
joseph isaac <josephisaac89c@gmail.com>
2019-06-03 10:23:32 -0700

    updates

--- src/hooks/useContextRouter.tsx
+++ src/hooks/useContextRouter.tsx
@@ -6,26 +6,25 @@ const defaultPageState = {
   isSlideIndex: 0
 };
 
+// listen to back button,  check to see if you can use multiple use effect
+
+const routeURLPath = Number(window.location.pathname.substr(window.location.pathname.length - 1));
+
 const useContextRouter = (route: string) => {
   const [state, setState] = useState(defaultPageState);
   const routeURL = `${config.baseURL}/${route}`;
 
   useEffect(() => {
-    const urlIndex = {
-      isSlideIndex: Number(window.location.pathname.substr(window.location.pathname.length - 1))
-    };
-
-    const nextState =
-      state.isSlideIndex !== defaultPageState.isSlideIndex
-        ? {
-            isSlideIndex: state.isSlideIndex
-          }
-        : urlIndex;
-    navigate(
-      `${state.isSlideIndex !== defaultPageState.isSlideIndex ? state.isSlideIndex : defaultPageState.isSlideIndex}`
-    );
-    setState(nextState);
+    if (routeURLPath > state.isSlideIndex) {
+      setSlideIndex(routeURLPath);
+    }
   }, [routeURL, state.isSlideIndex]);
+
+  useEffect(() => {
+    if (window.history && window.history.pushState) {
+      window.addEventListener('backbutton', () => setSlideIndex(routeURLPath - 1));
+    }
+  }, []);
 
   function setSlideIndex(n: number) {
     const nextState = {
@@ -34,6 +33,7 @@ const useContextRouter = (route: string) => {
     navigate(`${n}`);
     setState(nextState);
   }
+
   return {
     setSlideIndex,
     isSlideIndex: state.isSlideIndex
